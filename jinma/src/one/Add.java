b3f00edfd272393907a5319be537d129d3f3c794
Yestin <641010761@qq.com>
2017-06-18 19:16:45 +0800

    1
    符合题目规则

--- jinma/src/one/Add.java
+++ jinma/src/one/Add.java
@@ -7,14 +7,33 @@ public class Add {
 		return a+b;
 	}
 	public static void main(String[] args){
-		System.out.println("Number A£º");
-		Scanner a=new Scanner(System.in);
-		int numa=a.nextInt();
-		System.out.println("Number B£º");
-		Scanner b=new Scanner(System.in);
-		int numb=a.nextInt();
-		int sum=add(numa,numb);
-		System.out.println("The result is:"+sum+"¡£");
+		while(true){
+			Scanner a=new Scanner(System.in);
+			int numa=a.nextInt();
+			Scanner b=new Scanner(System.in);
+			int numb=a.nextInt();
+			int sum=add(numa,numb);
+			System.out.println(sum);
+		}
+		
 	}
 
 }
+
+/*
+C++°æ
+#include<iostream>
+using namspace std;
+int add(int a,int b)
+{
+	return a+b;
+}
+void main()
+{
+	int a,b,sum;
+	while(cin>>a>>b)
+	{
+		cout<<add(a,b)<<endl;
+	}
+}
+*/
